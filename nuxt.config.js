5bc68a2d7377b326cbc3a4fefbfcef5d49bb6adf
Renan Lima <renanlimabl@gmail.com>
2021-02-18 06:18:04 -0300

    add nuxt testing

---
+++ nuxt.config.js
@@ -0,0 +1,64 @@
+import axios from 'axios'
+
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt-fundamentals',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    '~/assets/style'
+  ],
+
+  generates: {
+    async routes () {
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+      return response.data.map(post => `posts/${post.id}`)
+      // return axios.get('https://jsonplaceholder.typicode.com/posts')
+      //   .then((response) => {
+      //     return response.data.map(post => `post/${post.id}`)
+      //   })
+    }
+  },
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    '@nuxtjs/eslint-module'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/axios'
+  ],
+
+  axios: {
+    baseURL: 'https://jsonplaceholder.typicode.com'
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/posts/_id.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="container">
+    <article>
+      <h1 class="title">
+        {{ post.title }}
+      </h1>
+      <p>{{ post.body }}</p>
+    </article>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ params, $axios, payload }) {
+    let post
+    if (payload) {
+      post = payload
+      console.log('getting post', post.id, 'from payload')
+    } else {
+      post = await $axios.$get(`posts/${params.id}`)
+      console.log('hitting the API for the post', post.id)
+    }
+    return { post }
+  },
+  data () {
+    return {
+      id: this.$route.params.id
+    }
+  },
+  head () {
+    return {
+      title: this.post.title,
+      meta: [
+        { name: 'twitter:title', content: this.post.title },
+        { name: 'twitter:description', content: this.post.body },
+        { name: 'twitter:image', content: 'https://i.imgur.com/UYP2umJ.png' },
+        { name: 'twitter:card', content: 'summary_large_image' }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    justify-content: space-between;
+    line-height: 1.5;
+  }
+  article * {
+    margin-bottom: 1rem;
+  }
+  aside {
+    min-width: 280px;
+    max-width: 280px;
+    padding-left: 2rem;
+  }
+  .title {
+    font-size: 2rem;
+  }
+</style>
